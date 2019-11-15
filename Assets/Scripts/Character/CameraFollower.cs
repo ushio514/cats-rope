2e9cad267e6dd90c17d9c73977f66b868b2becd8
Apache553 <Apache553@outlook.com>
2019-11-15 23:40:03 +0800

    fixed level 1 fixed some bug in player movement

---
+++ Assets/Scripts/Character/CameraFollower.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollower : MonoBehaviour
+{
+    public GameObject player;
+    public bool following = true;
+    private Vector3 offset;
+    private Vector3 tmp;
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+        offset = transform.position - player.transform.position;
+    }
+
+    void LateUpdate()
+    {
+        if (!following) return;
+        tmp = player.transform.position + offset;
+        //if (tmp.y > 1.0f) tmp.y = 1.0f;
+        transform.position = tmp;
+    }
+}
--- Assets/Scripts/Character/CharacterMovement.cs
+++ Assets/Scripts/Character/CharacterMovement.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CharacterMovement : MonoBehaviour
 {
@@ -9,12 +10,14 @@ public class CharacterMovement : MonoBehaviour
     private Transform trans = null;
     private SpriteRenderer rendererObject = null;
 
-    private const float speed = 4.0f;
+    private const float speed = 5.0f;
+    private bool isProcInput = true;
     private bool isHeadingRight = true;
     private bool isJumpping = false;
     private bool isReleasedJump = true;
     private bool isJumpPressed = false;
     private Vector2 velco = new Vector2();
+    private readonly Vector2 verVec = new Vector2(0.0f, 1.0f);
     public void Start()
     {
         playerObject = GameObject.FindWithTag("Player");
@@ -30,6 +33,7 @@ public class CharacterMovement : MonoBehaviour
 
     public void FixedUpdate()
     {
+        if (!isProcInput) return;
         float inXAxis = Input.GetAxis("Horizontal");
         float inYAxis = isJumpPressed ? 1.0f : 0.0f;
         float deltaX = inXAxis * speed;
@@ -55,13 +59,40 @@ public class CharacterMovement : MonoBehaviour
         rigidbodyObject.velocity = velco;
     }
 
-    public void OnCollisionEnter2D(Collision2D collision)
+    public void OnCollisionStay2D(Collision2D collision)
     {
+        foreach(ContactPoint2D cpoint in collision.contacts)
+        {
+            // Debug.DrawRay(cpoint.point, cpoint.normal, Color.red, 10.0f);
+            if (Mathf.Abs(Vector2.Dot(cpoint.normal, verVec)) < 0.0f) 
+            {
+                return;
+            }
+        }
         if (collision.gameObject.tag.Contains("Terrain"))
         {
+            if (rigidbodyObject.velocity.y != 0.0f) return;
             isJumpping = false;
-            Debug.Log("Touched"+Time.time.ToString());
+            // Debug.Log("Touched"+Time.time.ToString());
         }
     }
 
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag.Contains("Thorn"))
+        {
+            Debug.Log("DIED!");
+            isProcInput = false;
+            rigidbodyObject.velocity = new Vector2(0.0f, 7.0f);
+            playerObject.GetComponent<BoxCollider2D>().enabled = false;
+            GameObject.FindWithTag("MainCamera").GetComponent<CameraFollower>().following = false;
+            StartCoroutine("DeathDelay");
+        }
+    }
+
+    IEnumerator DeathDelay()
+    {
+        yield return new WaitForSeconds(2);
+        SceneManager.LoadScene("Start");
+    }
 }
